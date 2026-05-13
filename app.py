fe7260cbf754653b91beaaa453bf94badcf6fbb6
Carlos <misterstan7@gmail.com>
2026-05-13 10:38:55 -0300

    corrige erro de sintaxe

--- app.py
+++ app.py
@@ -517,4 +517,4 @@ import os
 if __name__ == "__main__":
     criar_banco()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port
+    app.run(host="0.0.0.0", port=port)
